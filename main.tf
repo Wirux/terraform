83c4b910cf9d2e1627842d8caf3bab563c4f7257
Adam <wiru97@gmail.com>
2021-09-20 19:41:49 +0200

    changed key

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "AKIAUMHIZWBQ2PYHFBDE"
-  secret_key = "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"
-
+  shared_credentials_file = "/Users/tf_user/.aws/credentials"
 }
-
 resource "aws_instance" "web" {
   ami           = "ami-05f7491af5eef733a"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "changeKey"
   }
 }
